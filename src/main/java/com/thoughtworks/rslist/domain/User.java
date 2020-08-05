828e9c7625ae29dd8d13b3d9387142339f903c98
Chen Sicong <sicong.chen@163.com>
2020-08-05 13:35:59 +0800

    [Chen Sicong] fix: some validated issues

--- src/main/java/com/thoughtworks/rslist/domain/User.java
+++ src/main/java/com/thoughtworks/rslist/domain/User.java
@@ -2,6 +2,7 @@ package com.thoughtworks.rslist.domain;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -14,9 +15,11 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @NotNull
+    @NotEmpty
     @Size(max = 8)
     private String name;
 
+    @Pattern(regexp = "(fe)?male")
     private String gender;
 
     @Max(100)
