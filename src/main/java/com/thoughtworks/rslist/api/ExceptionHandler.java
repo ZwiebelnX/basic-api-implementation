250c8b37540613b2f477b3e3017a7865d3b0b66d
Chen Sicong <sicong.chen@163.com>
2020-08-05 17:27:32 +0800

    [Chen Sicong] feat: add logger in ExceptionHandler

--- src/main/java/com/thoughtworks/rslist/api/ExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/api/ExceptionHandler.java
@@ -5,12 +5,16 @@ import com.thoughtworks.rslist.domain.RsEvent;
 import com.thoughtworks.rslist.domain.User;
 import com.thoughtworks.rslist.exception.CustomException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class ExceptionHandler {
+
+    Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
 
     @org.springframework.web.bind.annotation.ExceptionHandler(value = {MethodArgumentNotValidException.class, CustomException.class})
     public ResponseEntity<Error> exceptionHandler(Exception e) {
@@ -27,6 +31,7 @@ public class ExceptionHandler {
         if (e instanceof CustomException) {
             errMessage = ((CustomException) e).getErrorMessage();
         }
+        logger.error(errMessage);
         Error error = new Error();
         error.setError(errMessage);
         return ResponseEntity.badRequest().body(error);
